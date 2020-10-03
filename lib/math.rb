a8dd6b141cc609c8646f64422d8d745ac98f64b3
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-03 08:29:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,14 +4,19 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference=num2-num1
+p difference
 end
 
 def division(num1, num2)
+  quotient=num2/num1
+  p quotient
 
 end
 
 def multiplication(num1, num2)
+  product=num2*num1
+  p product
 
 end
 
