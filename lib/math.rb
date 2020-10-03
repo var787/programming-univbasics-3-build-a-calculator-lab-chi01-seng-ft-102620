fc4f8c0e08b60d4c59aef4e71adf4768ff7c72a8
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-03 08:35:14 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,32 @@
 def addition(num1, num2)
-
+  sum=num2+num1
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  difference=num1-num2
+p difference
 end
 
 def division(num1, num2)
+  quotient=num1/num2
+  p quotient
 
 end
 
 def multiplication(num1, num2)
+  product=num2*num1
+  p product
 
 end
 
 def modulo(num1, num2)
+  modu=num1%num2
+  p modu
 
 end
 
 def square_root(num)
-
+ sqr=Math.sqrt(num)
+ p sqr
 end
